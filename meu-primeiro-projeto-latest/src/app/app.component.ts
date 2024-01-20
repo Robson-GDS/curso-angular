e277592fd109be265ac1c40cf08dbd1036300da6
Robson <robson.080490@gmail.com>
2024-01-20 15:36:55 -0300

    git push -u origin main

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -1,13 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CommonModule, RouterOutlet, NewComponent],
+  template: `
+    <!-- <router-outlet /> -->
+    <h1>Curso de Angular</h1>
+    <app-new-component />
+  `
 })
-export class AppComponent {
-  title = 'meu-primeiro-projeto-latest';
-}
+export class AppComponent {}
