b40e887bd2b8921e5ec7f269532780928c89f8e8
Robson <robson.080490@gmail.com>
2024-01-21 19:44:36 -0300

    NgClass e NgStyle

--- meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-template-binding',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './template-binding.component.html',
   styleUrl: './template-binding.component.scss'
 })
