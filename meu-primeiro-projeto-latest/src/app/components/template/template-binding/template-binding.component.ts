866f63bd645029fe9353fa062b9a4ebf1ec5e460
Robson <robson.080490@gmail.com>
2024-01-21 13:38:55 -0300

    Property binding

--- meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.component.ts
@@ -11,6 +11,10 @@ export class TemplateBindingComponent {
   public name = 'Robson Gomers';
   public age = 33;
   public condition = this.age < 18 ? "Menor de idade" : "Maior de idade";
+
+  public isDisabled = false;
+  public srcValue = 'https://sm.ign.com/t/ign_br/screenshot/default/os-cavaleiros-do-zodiaco-na-tv_ejjr.1280.jpg';
+
   public sum(number1: number, number2: number) {
     return number1 + number2;
   }
