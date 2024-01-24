40cd29b6974b55e40d62518b204953ee3de18e43
Robson <robson.080490@gmail.com>
2024-01-23 22:59:25 -0300

    deferrable views

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { NewComponent } from './components/new-component/new-component.component
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
+import { TemplateDeferrableViewsComponent } from './components/template/template-deferrable-views/template-deferrable-views.component';
 
 @Component({
   selector: 'app-root',
@@ -16,15 +17,17 @@ import { TemplateControlFlowComponent } from './components/template/template-con
     NewComponent, 
     TemplateBindingComponent, 
     TemplateVariablesComponent, 
-    TemplateControlFlowComponent
+    TemplateControlFlowComponent,
+    TemplateDeferrableViewsComponent
   ],
   template: `
     <!-- <router-outlet /> -->
     <h1>Curso de Angular</h1>
     <!-- <app-new-component /> -->
     <!-- <app-template-binding />
-    <app-template-variables /> -->
-    <app-template-control-flow />
+    <!-- <app-template-variables /> -->
+    <!-- <app-template-control-flow /> -->
+    <app-template-deferrable-views />
   `
 })
 export class AppComponent {}
--- meu-primeiro-projeto-latest/src/app/components/template/template-control-flow/template-control-flow.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/template/template-control-flow/template-control-flow.component.ts
@@ -1,13 +1,21 @@
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule, NgFor, NgIf, NgSwitch } from '@angular/common';
 import { Component } from '@angular/core';
+import { Observable, delay, of } from 'rxjs';
 
 @Component({
   selector: 'app-template-control-flow',
   standalone: true,
-  imports: [CommonModule],
+  imports: [AsyncPipe, NgIf, NgFor, NgSwitch],
   templateUrl: './template-control-flow.component.html',
   styleUrl: './template-control-flow.component.scss'
 })
 export class TemplateControlFlowComponent {
   public isTrue = true;
+  public switchCondition = 'C';
+
+  public loadingData$: Observable<string[]> = of([
+    'item 1',
+    'item 2',
+    'item 3',
+  ]).pipe(delay(3000));
 }
